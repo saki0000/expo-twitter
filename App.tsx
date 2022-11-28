7087c887499c030ed60883b14ef9d1269313cc42
saki000 <hangchengtengqi@gmail.com>
2022-11-28 17:00:08 +0900

    ログインボタンの作成

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, NativeModules, StyleSheet, View } from "react-native";
+const { RNTwitterSignIn } = NativeModules;
 
 export default function App() {
+  const loginTwitter = () => {
+    RNTwitterSignIn.init();
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button onPress={loginTwitter} title="login" />
     </View>
   );
 }
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
