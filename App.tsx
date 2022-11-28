fa221badf51967e3f71f1e40c83cd52e5387d396
saki000 <hangchengtengqi@gmail.com>
2022-11-28 17:06:00 +0900

    apikeyを設定

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import { Button, NativeModules, StyleSheet, View } from "react-native";
-const { RNTwitterSignIn } = NativeModules;
+
+const twitterApi = {
+  key: "aWNG9Ngbw4S2qw1iM9VTvhNZr",
+  secret: "qfc5HA2bufW8aEu1QWProBKOJ2KseTcYFkprBUDq0AjEEfeef1",
+};
 
 export default function App() {
+  const { RNTwitterSignIn } = NativeModules;
   const loginTwitter = () => {
-    RNTwitterSignIn.init();
+    RNTwitterSignIn.init(twitterApi.key, twitterApi.secret);
+    RNTwitterSignIn.logIn();
   };
   return (
     <View style={styles.container}>
